951240bd5fe3a00b386422e5dba5726aa8da49d2
quazuo <filipglebocki01@gmail.com>
2024-08-06 17:52:40 +0200

    added rt shaders and updated the shader compilation script

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,20 +1,31 @@
-
 @echo off
 for /D %%i in (C:\VulkanSDK\*) do set "SDK_DIR=%%i"
 set "IS_ERROR=0"
 
-set shaders="main" "skybox" "prepass" "sphere-cube" "ss-quad" "ssao"
+set graphics_shaders="main" "skybox" "prepass" "sphere-cube" "ss-quad" "ssao"
+set rt_shaders="raytrace"
 
-(for %%a in (%shaders%) do (
-   @echo on
-   %SDK_DIR%/Bin/glslc.exe %%a.vert -o obj/%%a-vert.spv -g --target-env=vulkan1.1
-   @echo off
-   if %ERRORLEVEL% NEQ 0 set "IS_ERROR=1"
+set graphics_exts="vert" "frag"
+set rt_exts="rchit" "rgen" "rmiss"
 
-   @echo on
-   %SDK_DIR%/Bin/glslc.exe %%a.frag -o obj/%%a-frag.spv -g --target-env=vulkan1.1
-   @echo off
-   if %ERRORLEVEL% NEQ 0 set "IS_ERROR=1"
+set SPV_FLAGS=-g --target-env=vulkan1.2
+
+(for %%a in (%graphics_shaders%) do (
+    (for %%e in (%graphics_exts%) do (
+        @echo on
+        %SDK_DIR%/Bin/glslc.exe %%a.%%e -o obj/%%a-%%e.spv %SPV_FLAGS%
+        @echo off
+        if %ERRORLEVEL% NEQ 0 set "IS_ERROR=1"
+    ))
+))
+
+(for %%a in (%rt_shaders%) do (
+    (for %%e in (%rt_exts%) do (
+        @echo on
+        %SDK_DIR%/Bin/glslc.exe %%a.%%e -o obj/%%a-%%e.spv %SPV_FLAGS%
+        @echo off
+        if %ERRORLEVEL% NEQ 0 set "IS_ERROR=1"
+    ))
 ))
 
 if %IS_ERROR% NEQ 0 exit 1
