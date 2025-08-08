94d2d1449f7f5b999e8e20b571d46089b5f60d82
quazuo <filipglebocki01@gmail.com>
2025-08-08 16:56:08 +0200

    fixed barriers and cmake module support

--- src/render/renderer.cpp
+++ src/render/renderer.cpp
@@ -242,6 +242,8 @@ void VulkanRenderer::recreate_swap_chain() {
 
     wait_idle();
 
+    Logger::debug("recreating swap chain");
+
     swap_chain.reset();
     swap_chain = make_unique<SwapChain>(
         ctx,
@@ -1000,9 +1002,17 @@ void VulkanRenderer::record_pre_partition_commands(const vector<RenderNodeHandle
         constexpr auto new_layout = vk::ImageLayout::eShaderReadOnlyOptimal;
 
         barriers.insert(vk::ImageMemoryBarrier2 {
-            .srcStageMask = is_unbarriered_gfx ? vk::PipelineStageFlagBits2::eAllGraphics : vk::PipelineStageFlagBits2::eComputeShader,
-            .srcAccessMask = vk::AccessFlagBits2::eShaderWrite,
-            .dstStageMask = vk::PipelineStageFlagBits2::eAllGraphics,
+            .srcStageMask = is_unbarriered_gfx
+                            ? (is_depth_texture
+                                ? vk::PipelineStageFlagBits2::eEarlyFragmentTests | vk::PipelineStageFlagBits2::eLateFragmentTests
+                                : vk::PipelineStageFlagBits2::eColorAttachmentOutput)
+                            : vk::PipelineStageFlagBits2::eComputeShader,
+            .srcAccessMask = is_unbarriered_gfx
+                             ? (is_depth_texture
+                                 ? vk::AccessFlagBits2::eDepthStencilAttachmentWrite
+                                 : vk::AccessFlagBits2::eColorAttachmentWrite)
+                             : vk::AccessFlagBits2::eShaderWrite,
+            .dstStageMask = vk::PipelineStageFlagBits2::eFragmentShader,
             .dstAccessMask = vk::AccessFlagBits2::eShaderRead,
             .oldLayout = old_layout,
             .newLayout = new_layout,
